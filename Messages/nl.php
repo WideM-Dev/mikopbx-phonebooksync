b5c3472069312de8a30920594cf5ac93bd869935
WideM - Michiel <dev@widem.nl>
2026-06-06 20:51:13 +0200

    Rename to ModulePhoneBookSync v1.1.7

--- Messages/nl.php
+++ Messages/nl.php
@@ -1,8 +1,8 @@
 <?php
 // Cloud Phonebook — Nederlands
 return [
-    'BreadcrumbModulePhonebookSync' => 'Cloud Telefoonboek',
-    'SubHeaderModulePhonebookSync'  => 'Gecombineerd telefoonboek met interne toestellen en externe contacten',
+    'BreadcrumbModulePhoneBookSync' => 'Cloud Telefoonboek',
+    'SubHeaderModulePhoneBookSync'  => 'Gecombineerd telefoonboek met interne toestellen en externe contacten',
     'module_name'               => 'Cloud Telefoonboek',
     'module_description'        => 'Gecombineerd telefoonboek met interne toestellen en externe contacten',
     'phonebook_title'           => 'Cloud Telefoonboek',
