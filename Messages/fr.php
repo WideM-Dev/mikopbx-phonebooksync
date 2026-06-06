025751c8bf6b5aa3a4e81df1b66345dfd7506830
WideM - Michiel <dev@widem.nl>
2026-06-06 20:51:14 +0200

    Rename to ModulePhoneBookSync v1.1.7

--- Messages/fr.php
+++ Messages/fr.php
@@ -1,8 +1,8 @@
 <?php
 // Cloud Phonebook — Français
 return [
-    'BreadcrumbModulePhonebookSync' => 'Cloud Annuaire',
-    'SubHeaderModulePhonebookSync'  => 'Annuaire unifié combinant extensions internes et contacts externes',
+    'BreadcrumbModulePhoneBookSync' => 'Cloud Annuaire',
+    'SubHeaderModulePhoneBookSync'  => 'Annuaire unifié combinant extensions internes et contacts externes',
     'module_name'               => 'Cloud Annuaire',
     'module_description'        => 'Annuaire unifié combinant extensions internes et contacts externes',
     'phonebook_title'           => 'Cloud Annuaire',
